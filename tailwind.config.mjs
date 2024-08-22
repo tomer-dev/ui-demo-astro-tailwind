754f4a25f23ca17d81c51047d2b832b6dcb834d0
tomer-dev <tomer.sharoni@rookout.com>
2024-08-22 11:26:39 +0200

    content of contact us layout ready, navbar wip
    Signed-off-by: tomer-dev <tomer.sharoni@rookout.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "accent-light": "#4D9DE8",
+        "accent-dark": "#021E2F",
+        "custom-black": "#333333",
+      },
+      backgroundColor: {
+        "gray-button": "#69727d",
+        "custom-black": "#021E2F",
+      },
+      borderColor: {
+        "gray-input": "#69727d",
+      },
+    },
+    fontFamily: {
+      sans: ["Heebo"],
+      serif: [],
+    },
+  },
+  plugins: [],
+};
